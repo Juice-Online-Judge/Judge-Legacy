9b8fdbd8989553b22d1151842993139d35cf9300
DanSnow <dododavid006@gmail.com>
2014-05-25 23:53:50 +0800

    Update:Add basic compile

--- lib/dispatch.rb
+++ lib/dispatch.rb
@@ -5,3 +5,30 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'jimson'
+require 'pathname'
+
+class JudgeHandler
+  extend Jimson::Handler
+  C = 0
+  CPP = 1
+  def addJudge(type, pathStr)
+    path = Pathname.new(pathStr)
+    cmd = ""
+    res = "AC"
+    case type
+    when C
+      name = path.basename(".c")
+      cmd = "gcc -O2 -o #{path.dirname + name} #{pathStr} -lm"
+    when CPP
+      name = path.basename(".cpp")
+      cmd = "g++ -O2 -o #{name} #{path.basename} -lm"
+    else
+      return "Error:Unknow file type"
+    end
+    res = "CE" unless system cmd
+    return res
+  end
+end
+
+server = Jimson::Server.new(JudgeHandler.new, {host:"127.0.0.1", port:4242})
+server.start
