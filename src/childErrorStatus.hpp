1dca50f8aaf40c86e79be9a00b4c9eea7fccd3c0
DanSnow <dododavid006@gmail.com>
2014-06-14 22:18:21 +0800

    Update: Define error code

--- src/childErrorStatus.hpp
+++ src/childErrorStatus.hpp
@@ -1,7 +1,12 @@
 #ifndef CHILDERRORSTATUS_HPP_INCLUDE
 #define CHILDERRORSTATUS_HPP_INCLUDE
 
-
+enum ChildErrorStatus {
+  FileOpenError = 80,
+  Dup2Error = 81,
+  ExeclpError = 82,
+  InFileNotFound = 83
+};
 
 #endif /* end of include guard: CHILDERRORSTATUS_HPP_INCLUDE */
 
