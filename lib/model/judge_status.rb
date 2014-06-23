12e6bef785e8f02acb7db56116af10cc6f3b96b9
DanSnow <dododavid006@gmail.com>
2014-06-23 09:51:27 +0800

    Update:Add basic structure;Fix require path

--- lib/model/judge_status.rb
+++ lib/model/judge_status.rb
@@ -3,6 +3,14 @@
 
 require 'rubygems'
 
-require_relative '../config/environment.rb'
+require_relative '../../config/environment.rb'
 
+require 'sqlite3'
 require 'active_record'
+
+ActiveRecord::Base.establish_connection(adapter:"sqlite3", database:"#{AppPath}/run/judge.db")
+
+class Judge_Competition_Result < ActiveRecord::Base
+end
+
+p Judge_Competition_Result.all
