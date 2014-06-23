eb73944028b3dd7e49b7d774fb98f6ad0ad92d9e
DanSnow <dododavid006@gmail.com>
2014-06-23 09:51:00 +0800

    Update:Finish basic judge

--- lib/judge.rb
+++ lib/judge.rb
@@ -4,3 +4,15 @@
 require 'rubygems'
 
 require_relative '../config/environment.rb'
+
+class Judger
+  def Judget.judge(ques, ans)
+    out = File.read(File.join(AppPath, "run", "out", "#{ques}.out"))
+    out.each_line.zip(ans.each_line) { |x, y|
+      if x != y
+        return false
+      end
+    }
+    return true
+  end
+end
