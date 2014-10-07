64d7e275c8cf6ffdc8de267fb5cc58f0889879c5
DanSnow <dododavid006@gmail.com>
2014-10-07 17:13:54 +0800

    Update: Add dir make

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ cd $(dirname $0)
 scons
 mv build/executor bin
 mv build/libexecutor.so lib/executor
+mkdir -p "run/exe" > /dev/null 2>&1
+mkdir -p "run/in" > /dev/null 2>&1
+mkdir -p "run/out" > /dev/null 2>&1
+mkdir -p "run/ans" > /dev/null 2>&1
