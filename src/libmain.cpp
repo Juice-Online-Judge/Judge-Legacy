94ece75fb9b414f136a0f74d4eccf14c53619b32
DanSnow <dododavid006@gmail.com>
2014-12-13 10:51:50 +0800

    Update: Let logger only initialize once time

--- src/libmain.cpp
+++ src/libmain.cpp
@@ -1,9 +1,23 @@
 #include "execute.hpp"
 #include "logger.hpp"
 
-extern "C"
-int executor(char *root, char *ques, char *path, int sec, int mem) {
-  loggerInit(root);
-  BOOST_LOG_TRIVIAL(info) << "Root: " << root;
-  return execute(root, ques, path, sec, mem);
+#include <string>
+
+using namespace std;
+
+namespace {
+  string root;
+};
+
+extern "C" {
+  void executorInit(char *root) {
+    ::root = root;
+    loggerInit(root);
+    BOOST_LOG_TRIVIAL(info) << "Executor Initialize";
+    BOOST_LOG_TRIVIAL(info) << "Root: " << root;
+  }
+
+  int executor(char *ques, char *path, int sec, int mem) {
+    return execute(root, ques, path, sec, mem);
+  }
 }
